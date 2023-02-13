dda8337f9dd2b50aa007ad1a76ffbe6f3da95669
Izan Pérez Cosano <izanperez98@gmail.com>
2023-02-13 21:49:27 +0100

    day 17 part 2

--- day17/day17_2.js
+++ day17/day17_2.js
@@ -1,84 +1,56 @@
 /*
 *   --- Day 16: Pyroclastic Flow ---
-*           --- Part One ---
+*           --- Part Two ---
 *         Advent Of Code 2022
 * */
 
 const readline = require('readline')
 
-function askQuestion(query) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    return new Promise(resolve => rl.question(query, ans => {
-        rl.close();
-        resolve(ans);
-    }))
-}
-
-const debug = false
-
 let jetPattern
 const lineReader = readline.createInterface({
     input: require('fs').createReadStream('./day17.txt')
 })
-
 lineReader.on('line', (line) => jetPattern = line).on('close', () => simulation())
 
 let room = []
-let total = 0
+let offset = 0
+let cache = new Map()
+const TOTAL_ROCKS = 1_000_000_000_000
 
 async function simulation() {
-    for (let i = 0, n = 0; i < 1000000000000; i++) {
-        let rock = getRock(i % 5)
+    for (let i = 0, n = 0; i < TOTAL_ROCKS; i++) {
+        let rockIndex = i % 5
+        let rock = getRock(rockIndex)
         addThreeSpacesUp()
-        if (debug) {
-            console.log(`The #${i + 1} rock begins to falling`)
-            print(rock)
-            print(room)
-            await askQuestion('Continue')
-            console.log('\033[2J');
-        }
         while (true) {
-            const direction = jetPattern[n++ % jetPattern.length]
+            const jetIndex = n++ % jetPattern.length
+            const direction = jetPattern[jetIndex]
             if (canMoveToSide(direction, rock)) moveToSide(direction, rock)
-            if (debug) {
-                print(rock)
-                print(room)
-                await askQuestion('Continue')
-                console.log('\033[2J');
-            }
             if (canMoveDown(rock)) {
-                if (debug) console.log('Rock falls 1 unit')
                 moveDown(rock)
-                if (debug) {
-                    print(rock)
-                    print(room)
-                    await askQuestion('Continue')
-                    console.log('\033[2J');
-                }
             } else {
-                if (debug) console.log('Causing it to come to rest')
                 const restingRock = rock.map(row => row.map(item => item.replace('@', '#')))
                 for (let i = restingRock.length - 1; i >= 0; i--) {
                     room.unshift(restingRock[i])
                 }
-                checkPrune()
-                if (debug) {
-                    print(room)
-                    await askQuestion('Continue')
-                    console.log('\033[2J');
+                const state = [jetIndex, rockIndex, getTop()].toString()
+                if (cache.has(state)) {
+                    let [stateIteration, stateHeight] = cache.get(state)
+                    let remainingIterations = TOTAL_ROCKS - i
+                    let repetition = Math.floor(remainingIterations / (i - stateIteration))
+                    offset = repetition * (room.length - stateHeight)
+                    i += repetition * (i - stateIteration)
+                    cache = new Map()
                 }
+                cache.set(state, [i, room.length])
                 break
             }
         }
     }
-    console.log(room.length + total)
+    console.log(room.length + offset)
 }
 
-function checkPrune() {
+function getTop() {
     let levels = []
     for (let i = 0; i < 7; i++) {
         const col = room.map(row => row[i])
@@ -86,11 +58,7 @@ function checkPrune() {
         levels.push(topEdge)
     }
     const max = Math.max(...levels)
-    if (!levels.some(level => level === -1)) {
-        total += room.length - max - 1
-        room = room.slice(0, max + 1)
-    }
-
+    return levels.map(level => level - max)
 }
 
 function addThreeSpacesUp() {
@@ -140,11 +108,8 @@ function canMoveDown(rock) {
     if (rock.length > 1) {
         const midRock = rock[rock.length - 2]
         secondLayer = !bottomRock.some((point, i) => point === '#' && midRock[i] === '@')
-        if (debug) console.log(midRock.join(''), secondLayer)
     }
     const firstLayer = !topRoom.some((point, i) => point === '#' && bottomRock[i] === '@')
-    if (debug) console.log(bottomRock.join(''), firstLayer)
-    if (debug) console.log(topRoom.join(''))
     return firstLayer && secondLayer
 }
 
@@ -164,27 +129,22 @@ function moveDown(rock) {
 
 function canMoveToSide(direction, rock) {
     if (direction === '<') {
-        let res = rock.every(row => {
+        return rock.every(row => {
             const leftEdge = row.indexOf('@')
             if (leftEdge < 0 && row.indexOf('#') >= 0) return true
             return leftEdge > 0 && row[leftEdge - 1] === '.'
         })
-        if (!res && debug) console.log('Jet of gas pushes rock left, but nothing happens')
-        return res
     } else {
-        let res = rock.every(row => {
+        return rock.every(row => {
             const rightEdge = row.length - row.slice().reverse().indexOf('@') - 1
             if (row.indexOf('@') < 0 && row.indexOf('#') >= 0) return true
             return rightEdge < (row.length - 1) && row[rightEdge + 1] === '.'
         })
-        if (!res && debug) console.log('Jet of gas pushes rock right, but nothing happens')
-        return res
     }
 }
 
 function moveToSide(direction, rock) {
     if (direction === '<') {
-        if (debug) console.log('Jet of gas pushes rock left')
         return rock.map(row => {
             const leftEdge = row.indexOf('@')
             if (leftEdge < 0 && row.indexOf('#') >= 0) return row
@@ -194,7 +154,6 @@ function moveToSide(direction, rock) {
             return row
         })
     } else {
-        if (debug) console.log('Jet of gas pushes rock right')
         return rock.every(row => {
             const leftEdge = row.indexOf('@')
             if (leftEdge < 0 && row.indexOf('#') >= 0) return row
@@ -204,8 +163,4 @@ function moveToSide(direction, rock) {
             return row
         })
     }
-}
-
-function print(element) {
-    element.slice(0, 10).map(row => row.join('')).forEach(row => console.log(row))
 }
