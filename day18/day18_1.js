b00437491e6c4e70e35e4b5ba39bfc65867b521c
Ethan <izanperez98@gmail.com>
2023-02-14 14:48:46 +0100

    day 18 part 1

--- day18/day18_1.js
+++ day18/day18_1.js
@@ -4,39 +4,35 @@
 *         Advent Of Code 2022
 * */
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./test.txt')
+    input: require('fs').createReadStream('./day18.txt')
 })
-// 1,1,1 and 2,1,1 is 10
-// Test input is 64
 
 const grid = []
 
 lineReader.on('line', line => {
     const [x, y, z] = line.split(',').map(item => parseInt(item))
     grid.push({x, y, z})
-}).on('close', () => test())
+}).on('close', () => countSides())
 
 function isAdjacentX(item) {
-    return grid.find(cube => (Math.abs(cube.x - item.x) === 1) && cube.y === item.y && cube.z === item.z)
+    return grid.find(cube => cube.x === item.x + 1 && cube.y === item.y && cube.z === item.z)
 }
 
 function isAdjacentY(item) {
-    return grid.find(cube => cube.x === item.x && (Math.abs(cube.y - item.y) === 1) && cube.z === item.z)
+    return grid.find(cube => cube.x === item.x && cube.y === item.y + 1 && cube.z === item.z)
 }
 
 function isAdjacentZ(item) {
-    return grid.find(cube => cube.x === item.x && cube.y === item.y && (Math.abs(cube.z - item.z) === 1))
+    return grid.find(cube => cube.x === item.x && cube.y === item.y && cube.z === item.z + 1)
 }
 
-
-function test() {
-    // TEST
-    //console.log(grid)
+function countSides() {
     let totalSides = grid.length * 6
-    console.log(totalSides)
     for (const cube of grid) {
-        if (isAdjacentX(cube) || isAdjacentY(cube) || isAdjacentZ(cube)) totalSides -= 2
+        if (isAdjacentX(cube)) totalSides -= 2
+        if (isAdjacentY(cube)) totalSides -= 2
+        if (isAdjacentZ(cube)) totalSides -= 2
     }
-    console.log(totalSides)
-
+    console.log('Total:', totalSides)
+    // Total: 3390
 }
