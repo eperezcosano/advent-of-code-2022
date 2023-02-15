02f73b3c5253006b8c24e2f2fa0081deaad66aa4
Izan Pérez Cosano <izanperez98@gmail.com>
2023-02-15 19:53:56 +0100

    day 19 init

---
+++ day19/day19_1.js
@@ -0,0 +1,8 @@
+/*
+*   --- Day 19: Not Enough Minerals ---
+*            --- Part One ---
+*          Advent Of Code 2022
+* */
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day19.txt')
+})
