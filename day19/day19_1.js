d46186ab16693b3b2727a8b16f4f251bef247195
Ethan <izanperez98@gmail.com>
2023-02-16 16:12:27 +0100

    day 19 part 1 dev

--- day19/day19_1.js
+++ day19/day19_1.js
@@ -7,67 +7,63 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./test.txt')
 })
 
-const blueprints = []
+let qualityLevel = 0
 
-function executeBlueprint(id) {
-    const blueprint = blueprints[id - 1]
-    const inventory = {
-        oreRobots: 1,
-        ores: 0,
-        clayRobots: 0,
-        clay: 0,
-        obsidianRobots: 0,
-        obsidian: 0,
-        geodeRobots: 0,
-        geodes: 0
-    }
-    for (let t = 1; t <= 24; t++) {
-        let buildClay = false
-        let buildObsidian = false
-        let buildGeode = false
-        console.log(`== Minute ${t} ==`)
-        if (blueprint.clayCost <= inventory.ores) {
-            inventory.ores -= blueprint.clayCost
-            buildClay = true
-            console.log(`Spend ${blueprint.clayCost} ore to start building a clay-collecting robot.`)
+function dfs(blueprint, maxCost, cache, time, robots, minerals) {
+    if (time === 0) return minerals.geode
+    const key = [time, ...Object.values(robots), ...Object.values(minerals)].toString()
+    if (cache.has(key)) return cache.get(key)
+    let maxGeodes = minerals.geode + robots.geode * time
+
+    for (const [robotType, robotCost] of Object.entries(blueprint)) {
+        if (robotType !== 'geode' && robots[robotType] >= maxCost[robotType]) continue
+
+        let maxWait = 0
+        let canBuild = true
+        for (const [mineralType, mineralAmount] of Object.entries(robotCost)) {
+            if (robots[mineralType] === 0) {
+                canBuild = false
+                break
+            }
+            const wait = Math.ceil((mineralAmount - minerals[mineralType]) / robots[mineralType])
+            maxWait = Math.max(maxWait, wait)
         }
-        if (blueprint.obsidianCostOre <= inventory.ores && blueprint.obsidianCostClay <= inventory.clay) {
-            inventory.ores -= blueprint.obsidianCostOre
-            inventory.clay -= blueprint.obsidianCostClay
-            buildObsidian = true
-            console.log(`Spend ${blueprint.obsidianCostOre} ore and ${blueprint.obsidianCostClay} clay to start building an obsidian-collecting robot.`)
-        }
-        if (blueprint.geodeCostOre <= inventory.ores && blueprint.geoCostObsidian <= inventory.obsidian) {
-            inventory.ores -= blueprint.geodeCostOre
-            inventory.obsidian -= blueprint.geoCostObsidian
-            buildGeode = true
-            console.log(`Spend ${blueprint.geodeCostOre} ore and ${blueprint.geoCostObsidian} obsidian to start building a geode-cracking robot.`)
-        }
-        // Collect minerals
-        inventory.ores += inventory.oreRobots
-        console.log(`${inventory.oreRobots} ore-collecting robot collects ${inventory.oreRobots} ore; you now have ${inventory.ores} ore.`)
-        inventory.clay += inventory.clayRobots
-        console.log(`${inventory.clayRobots} clay-collecting robot collects ${inventory.clayRobots} clay; you now have ${inventory.clay} clay.`)
-        inventory.obsidian += inventory.obsidianRobots
-        console.log(`${inventory.obsidianRobots} obsidian-collecting robot collects ${inventory.obsidianRobots} obsidian; you now have ${inventory.obsidian} obsidian.`)
-        inventory.geodes += inventory.geodeRobots
-        console.log(`${inventory.geodeRobots} geode-cracking robot crack ${inventory.geodeRobots} geodes; you now have ${inventory.geodes} geodes.`)
-        if (buildClay) {
-            inventory.clayRobots++
-            console.log(`The new clay-collecting robot is ready; you now have ${inventory.clayRobots} of them`)
-        }
-        if (buildObsidian) {
-            inventory.obsidianRobots++
-            console.log(`The new obsidian-collecting robot is ready; you now have ${inventory.obsidianRobots} of them`)
-        }
-        if (buildGeode) {
-            inventory.geodeRobots++
-            console.log(`The new geode-cracking robot is ready; you now have ${inventory.geodeRobots} of them`)
+        if (canBuild) {
+            const remainingTime = time - maxWait - 1
+            if (remainingTime <= 0) continue
+            const tmpRobots = {...robots}
+            const tmpMinerals = {... minerals}
+            Object.keys(tmpMinerals).forEach((mineralType, mineralAmount) => {
+                tmpMinerals[mineralType] = mineralAmount + robots[mineralType] * (maxWait + 1)
+            })
+            for (const [mineralType, mineralAmount] of Object.entries(robotCost)) {
+                tmpMinerals[mineralType] -= mineralAmount
+            }
+            tmpRobots[robotType]++
+            for (const mineralType of Object.entries(maxCost)) {
+                tmpMinerals[mineralType] = Math.min(tmpMinerals[mineralType], maxCost[mineralType] * remainingTime)
+            }
+            maxGeodes = Math.max(maxGeodes, dfs(blueprint, maxCost, cache, remainingTime, tmpRobots, tmpMinerals))
         }
     }
+    cache.set(key, maxGeodes)
+    return maxGeodes
 }
 
 lineReader.on('line', (line) => {
-    const [, oreCost, clayCost, obsidianCostOre, obsidianCostClay, geodeCostOre, geoCostObsidian] = line.split(' ').filter(word => word.match(/\d+/)).map(number => parseInt(number))
-    blueprints.push({oreCost, clayCost, obsidianCostOre, obsidianCostClay, geodeCostOre, geoCostObsidian})
-}).on('close', () => executeBlueprint(1))
+    const [id, oreCost, clayCost, obsidianCostOre, obsidianCostClay, geodeCostOre, geoCostObsidian] = line.split(' ').filter(word => word.match(/\d+/)).map(number => parseInt(number))
+    const maxCost = {
+        ore: Math.max(oreCost, clayCost, obsidianCostOre, geodeCostOre),
+        clay: obsidianCostClay,
+        obsidian: geoCostObsidian
+    }
+    const blueprint = {
+        ore: {ore: oreCost},
+        clay: {ore: clayCost},
+        obsidian: {ore: obsidianCostOre, clay: obsidianCostClay},
+        geode: {ore: geodeCostOre, obsidian: geoCostObsidian}
+    }
+    const maxGeodes = dfs(blueprint, maxCost, new Map(), 24, {ore: 1, clay: 0, obsidian: 0, geode: 0}, {ore: 0, clay: 0, obsidian: 0, geode: 0})
+    console.log(id, maxGeodes)
+    qualityLevel += id * maxGeodes
+}).on('close', () => console.log(qualityLevel))
