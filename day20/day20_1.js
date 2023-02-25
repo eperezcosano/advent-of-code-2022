6ab6732e53600c6bb7803fd4a6ea8f0866f205c4
Ethan <izanperez98@gmail.com>
2023-02-25 21:05:07 +0100

    day20 init

---
+++ day20/day20_1.js
@@ -0,0 +1,8 @@
+/*
+*   --- Day 20: Grove Positioning System ---
+*               --- Part One ---
+*             Advent Of Code 2022
+* */
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./test.txt')
+})
