d388e07737256267f307e1cee68fb4dbc73b1ab0
Ethan <izanperez98@gmail.com>
2023-03-02 15:10:35 +0100

    day21 dev init

---
+++ day21/day21_1.js
@@ -0,0 +1,8 @@
+/*
+*           --- Day 21: Monkey Math ---
+*               --- Part One ---
+*             Advent Of Code 2022
+* */
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day20.txt')
+})
