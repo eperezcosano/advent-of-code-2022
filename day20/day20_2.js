e5e3f429d6b5304ea4695f04736cc9eb0f5ccc73
Ethan <izanperez98@gmail.com>
2023-03-02 15:01:24 +0100

    day20 part two

--- day20/day20_2.js
+++ day20/day20_2.js
@@ -1,6 +1,6 @@
 /*
 *   --- Day 20: Grove Positioning System ---
-*               --- Part One ---
+*               --- Part Two ---
 *             Advent Of Code 2022
 * */
 const lineReader = require('readline').createInterface({
@@ -8,6 +8,7 @@ const lineReader = require('readline').createInterface({
 })
 
 let total = 0
+const decKey = 811589153
 const file = []
 
 function getCoordinates() {
@@ -17,14 +18,16 @@ function getCoordinates() {
 
 function mix() {
     const numbers = file.map(item => item.num)
-    for (let i = 0; i < numbers.length; i++) {
-        const id = file.findIndex(item => item.id === i)
-        file.splice(id, 1)
-        const newId = (numbers[i] + id) % file.length
-        file.splice(newId, 0, {num: numbers[i], id: i})
+    for (let n = 0; n < 10; n++) {
+        for (let i = 0; i < numbers.length; i++) {
+            const id = file.findIndex(item => item.id === i)
+            file.splice(id, 1)
+            const newId = (numbers[i] + id) % file.length
+            file.splice(newId, 0, {num: numbers[i], id: i})
+        }
     }
     console.log('Result:', getCoordinates())
-    // Result: 3473
+    // Result: 7496649006261
 }
 
-lineReader.on('line', (line) => file.push({num: parseInt(line), id: total++})).on('close', () => mix())
+lineReader.on('line', (line) => file.push({num: parseInt(line) * decKey, id: total++})).on('close', () => mix())
