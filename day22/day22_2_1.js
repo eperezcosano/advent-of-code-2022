cfb1c69780ff5181b19c28d51d0904bcf4535d78
Izan Pérez Cosano <izanperez98@gmail.com>
2023-03-09 17:50:34 +0100

    day 22 dev refractor

--- day22/day22_2_1.js
+++ day22/day22_2_1.js
@@ -4,12 +4,12 @@
 *             Advent Of Code 2022
 * */
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./day22.txt')
+    input: require('fs').createReadStream('./test2.txt')
 })
 
-const side = 50
+const side = 4
 const maze = []
-let movements = []
+const movements = []
 let space = false
 let pos = [0, side * 2]
 let facing = 0
@@ -20,59 +20,14 @@ let facing = 0
     Up:     3
  */
 
-lineReader.on('line', line => {
-    if (!line) {
-        space = true
-        return
-    }
-    space ? movements = line.match(/\d+|[LR]/g) : maze.push(line.split(''))
-}).on('close', () => simulation())
-
 function test() {
     //console.log('Start', pos, maze[pos[0]][pos[1]])
     for (let i = 0; i < 20; i++) {
         pos = [8, 15]
         facing = 3
-        moveUp(i)
+        move(i)
     }
 }
-
-function simulation() {
-    for (let i = 0; i < movements.length; i++) {
-        const steps = parseInt(movements[i])
-        console.log('Current pos', pos, facing, maze[pos[0]][pos[1]])
-        if (steps) {
-            console.log('Move', facing, steps)
-            // Move to the facing direction the number of steps
-            switch (facing) {
-                case 0:
-                    moveRight(steps)
-                    break
-                case 1:
-                    moveDown(steps)
-                    break
-                case 2:
-                    moveLeft(steps)
-                    break
-                case 3:
-                    moveUp(steps)
-                    break
-            }
-        } else {
-            console.log('Turn', movements[i])
-            // Turn clockwise (R) or counterclockwise (L)
-            if (movements[i] === 'R') facing = mod(facing + 1, 4)
-            else if (movements[i] === 'L') facing = mod(facing - 1, 4)
-        }
-    }
-    console.log(pos, facing)
-    console.log(password())
-}
-
-function password() {
-    return (pos[0] + 1) * 1000 + 4 * (pos[1] + 1) + facing
-}
-
 function mod(n, m) {
     return ((n % m) + m) % m
 }
@@ -122,55 +77,44 @@ function getCol() {
         return [...fourthCol, ...midRow]
     }
 }
-
-function moveRight(steps) {
-    const row = getRow()
-    let neighbor = row.findIndex(item => item[1][0] === pos[0] && item[1][1] === pos[1])
-    // console.log(row, neighbor, row[start], row.map(item => item[0]).join(''))
+function move(steps) {
+    const line = (facing % 2 === 0) ? getRow() : getCol()
+    const direction = (facing < 2) ? 1 : -1
+    let neighbor = line.findIndex(item => item[1][0] === pos[0] && item[1][1] === pos[1])
+    // console.log(line, neighbor, line[neighbor], line.map(item => item[0]).line(''))
     for (let i = 0; i < steps; i++) {
-        neighbor = mod(neighbor + 1, row.length)
-        if (row[neighbor][0] === '#') break
-        pos = row[neighbor][1]
-        facing = row[neighbor][2]
+        neighbor = mod(neighbor + direction, line.length)
+        if (line[neighbor][0] === '#') break
+        pos = line[neighbor][1]
+        facing = line[neighbor][2]
     }
     console.log(pos, maze[pos[0]][pos[1]], facing)
 }
 
-function moveLeft(steps) {
-    const row = getRow()
-    let neighbor = row.findIndex(item => item[1][0] === pos[0] && item[1][1] === pos[1])
-    //console.log(row, neighbor, row[neighbor], row.map(item => item[0]).join(''))
-    for (let i = 0; i < steps; i++) {
-        neighbor = mod(neighbor - 1, row.length)
-        if (row[neighbor][0] === '#') break
-        pos = row[neighbor][1]
-        facing = row[neighbor][2]
+function simulation() {
+    for (let i = 0; i < movements.length; i++) {
+        const steps = parseInt(movements[i])
+        console.log('Current pos', pos, facing, maze[pos[0]][pos[1]])
+        if (steps) {
+            console.log('Move', facing, steps)
+            // Move to the facing direction the number of steps
+            move(steps)
+        } else {
+            console.log('Turn', movements[i])
+            // Turn clockwise (R) or counterclockwise (L)
+            if (movements[i] === 'R') facing = mod(facing + 1, 4)
+            else if (movements[i] === 'L') facing = mod(facing - 1, 4)
+        }
     }
-    console.log(pos, maze[pos[0]][pos[1]], facing)
+    console.log(pos, facing)
+    console.log((pos[0] + 1) * 1000 + 4 * (pos[1] + 1) + facing)
 }
 
-function moveUp(steps) {
-    const col = getCol()
-    let neighbor = col.findIndex(item => item[1][0] === pos[0] && item[1][1] === pos[1])
-    console.log(col, neighbor, col[neighbor], col.map(item => item[0]).join(''))
-    for (let i = 0; i < steps; i++) {
-        neighbor = mod(neighbor - 1, col.length)
-        if (col[neighbor][0] === '#') break
-        pos = col[neighbor][1]
-        facing = col[neighbor][2]
+lineReader.on('line', line => {
+    if (!line) {
+        space = true
+        return
     }
-    console.log(pos, maze[pos[0]][pos[1]], facing)
-}
+    space ? movements.push(...line.match(/\d+|[LR]/g)) : maze.push(line.split(''))
+}).on('close', () => simulation())
 
-function moveDown(steps) {
-    const col = getCol()
-    let neighbor = col.findIndex(item => item[1][0] === pos[0] && item[1][1] === pos[1])
-    // console.log(col, neighbor, col[neighbor], col.map(item => item[0]).join(''))
-    for (let i = 0; i < steps; i++) {
-        neighbor = mod(neighbor + 1, col.length)
-        if (col[neighbor][0] === '#') break
-        pos = col[neighbor][1]
-        facing = col[neighbor][2]
-    }
-    //console.log(pos, maze[pos[0]][pos[1]], facing)
-}
