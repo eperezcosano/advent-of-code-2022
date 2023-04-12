e0cd00659543f436494e38a4465589031439af27
Izan Pérez Cosano <izanperez98@gmail.com>
2023-04-12 16:52:14 +0200

    day 23 init

---
+++ day23/day23_1.js
@@ -0,0 +1,8 @@
+/*
+*       --- Day 23: Unstable Diffusion ---
+*               --- Part One ---
+*             Advent Of Code 2022
+* */
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./sample.txt')
+})
---
+++ day23/day23_2.js
@@ -0,0 +1,8 @@
+/*
+*       --- Day 23: Unstable Diffusion ---
+*               --- Part Two ---
+*             Advent Of Code 2022
+* */
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./sample.txt')
+})
