15d650d798de28eba0e31ad8e6d717683b9bc47b
Izan Pérez Cosano <izanperez98@gmail.com>
2023-04-13 00:57:39 +0200

    day24 init

---
+++ day24/day24_1.js
@@ -0,0 +1,8 @@
+/*
+*       --- Day 24: Blizzard Basin ---
+*               --- Part One ---
+*             Advent Of Code 2022
+* */
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./sample.txt')
+})
---
+++ day24/day24_2.js
@@ -0,0 +1,8 @@
+/*
+*       --- Day 24: Blizzard Basin ---
+*               --- Part Two ---
+*             Advent Of Code 2022
+* */
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./sample.txt')
+})
