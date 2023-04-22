d6cc73506bb5fb70f1eeb0ffae19972f4c8de86a
Ethan <izanperez98@gmail.com>
2023-04-22 16:46:47 +0200

    day25 part one

--- day25/day25_1.js
+++ day25/day25_1.js
@@ -4,11 +4,35 @@
 *             Advent Of Code 2022
 * */
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./sample.txt')
+    input: require('fs').createReadStream('./day25.txt')
 })
 
-function simulation() {}
+let sum = 0
+
+function totalSum(snafu) {
+    let exp = 1
+    for (const x of snafu.split('').slice().reverse()) {
+        sum += (['=', '-', '0', '1', '2'].indexOf(x) - 2) * exp
+        exp *= 5
+    }
+}
+
+function toSnafu() {
+    let output = ''
+    while (sum) {
+        let remainder = sum % 5
+        sum = Math.floor(sum / 5)
+        if (remainder <= 2)
+            output = remainder.toString() + output
+        else {
+            output = "   =-"[remainder] + output
+            sum++
+        }
+    }
+    return output
+}
 
 lineReader.on('line', line => {
-    //grid.push(line.split(''))
-}).on('close', () => simulation())
+    totalSum(line)
+}).on('close', () => console.log('Result:', toSnafu()))
+// Result: 2011-=2=-1020-1===-1
