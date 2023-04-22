49328d58e537edafc6f246361163843d74725164
Ethan <izanperez98@gmail.com>
2023-04-22 14:44:50 +0200

    day24 part1 final

--- day24/day24_1.js
+++ day24/day24_1.js
@@ -3,23 +3,18 @@
 *               --- Part One ---
 *             Advent Of Code 2022
 * */
-const {lcm} = require('mathjs')
+const {lcm, mod} = require('mathjs')
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./sample.txt')
+    input: require('fs').createReadStream('./day24.txt')
 })
 
 const grid = []
 const blizzards = Array.from({ length: 4 }, () => new Set())
 
-function mod(n, m) {
-    return ((n % m) + m) % m
-}
-
 function bfs() {
     const maxY = grid.length - 1
     const maxX = grid[0].length - 1
     const repetition = lcm(maxY, maxX)
-    console.log(maxY, maxX, repetition)
     const queue = []
     const seen = new Set()
     queue.push([0, -1, 0])
@@ -28,23 +23,24 @@ function bfs() {
         time++
         for (const [dy, dx] of [[0, 1], [0, -1], [-1, 0], [1, 0], [0, 0]]) {
             const [nextY, nextX] = [y + dy, x + dx]
-
-            if (nextY === maxY && nextX === maxX - 1) return time
-
-            if ((nextY < 1 || nextX < 1 || nextY > maxY || nextX > maxX) && (nextY !== 0 && nextX !== 1)) continue
+            if (nextY === maxY && nextX === maxX - 1)
+                return time
+            if ((nextY < 0 || nextX < 0 || nextY >= maxY || nextX >= maxX) && !(nextY === -1 && nextX === 0))
+                continue
 
             let canMove = true
-            if (nextY !== -1 && nextX !== 0) {
+            if (!(nextY === -1 && nextX === 0)) {
                 for (const [direction, [testY, testX]] of [[0, -1], [0, 1], [-1, 0], [1, 0]].entries()) {
-                    const state = [mod(nextY - testY * time, maxY), mod(nextX - testX * time, maxX)]
-                    if (blizzards[direction].has(state.join('-'))) {
+                    const stateY = mod(nextY - testY * time, maxY)
+                    const stateX = mod(nextX - testX * time, maxX)
+                    if (blizzards[direction].has(`${stateY}-${stateX}`)) {
                         canMove = false
                         break
                     }
                 }
             }
             if (canMove) {
-                const key = [nextY, nextX, time % repetition].join('-')
+                const key = [nextY, nextX, mod(time, repetition)].join('-')
                 if (seen.has(key)) continue
                 seen.add(key)
                 queue.push([time, nextY, nextX])
@@ -67,15 +63,11 @@ function mapBlizzard(y, x) {
 }
 
 function mapBlizzards() {
-    for (let y = 0; y < grid.length; y++) {
-        for (let x = 0; x < grid[y].length; x++) {
+    for (let y = 0; y < grid.length - 1; y++) {
+        for (let x = 0; x < grid[y].length - 1; x++) {
             if (isBlizzard(grid[y][x])) mapBlizzard(y, x)
         }
     }
-}
-
-function printGrid() {
-    grid.forEach(row => console.log(row.join('')))
 }
 
 function cropGrid() {
@@ -86,7 +78,8 @@ function cropGrid() {
 function simulation() {
     cropGrid()
     mapBlizzards()
-    console.log(bfs())
+    console.log('Result:', bfs())
+    // Result: 247
 }
 
 lineReader.on('line', line => {
